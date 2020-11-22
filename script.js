807bd31045697a6801c075e7cbc51b61d31b1b22
Ofree <ofree21@gmail.com>
2020-11-22 17:29:03 +0200

    added functionality to the buttons

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+
+
 // Get Quote From API
 async function getQuote() {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
@@ -5,12 +12,37 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+        // If author is blank. add 'Unknown'
+        if(data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quotes
+        if(data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+
     } catch (error) {
         getQuote();
-        console.log('whoops, no quote', error);
     }
 }
 
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteButton.addEventListener('click', getQuote);
+twitterButton.addEventListener('click', tweetQuote);
+
 // On Load
-getQuote()
+// getQuote()
+
